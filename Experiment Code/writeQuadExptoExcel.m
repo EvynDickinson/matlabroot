6b1aad38d7da0f8b53ff685b8e0b249317a73d28
EvynDickinson <evyndickinson@gmail.com>
2021-11-01 11:42:50 -0400

    gui work

---
+++ Experiment Code/writeQuadExptoExcel.m
@@ -0,0 +1,47 @@
+function writeQuadExptoExcel(params)
+% Write the experiment data into Excel master sheet
+% single line for each arena, since they can function independently or
+% grouped
+% ES Dickinson
+
+% load current excel file:
+[excelfile, Excel, xlFile] = load_QuadBowlExperiments;
+sheet = 'Exp List'; % excel sheet to write into       
+fprintf('\n Writing to excel...')
+
+% Newest row to populate:
+nrow = size(excelfile,1)+1;
+
+% params.well_1 
+% params.well_2
+% params.well_3 
+% params.well_4 
+% params.date 
+% params.expID
+% params.genotype
+% params.protocol 
+% params.num 
+% params.arena
+
+base_paramList = {'date', 'expID', 'protocol'};
+arena_paramList = {'genotype','well_1', 'well_2', 'well_3', 'well_4'};
+         
+for arena = 1:4
+    % arena:
+    xlswrite(xlFile, {Alphabet(arena)}, sheet, [Alphabet(Excel.arena),num2str(nrow)]);
+    % shared data:
+    for ii = 1:length(base_paramList)
+        xlswrite(xlFile, {params.(base_paramList{ii})}, sheet, [Alphabet(Excel.(base_paramList{ii})),num2str(nrow)]);
+    end
+    % arena specific data:
+    for ii = 1:length(arena_paramList)
+        xlswrite(xlFile, {params.(['Arena' Alphabet(arena)]).(arena_paramList{ii})},...
+                 sheet, [Alphabet(Excel.(arena_paramList{ii})),num2str(nrow)]);
+    end
+    nrow = nrow+1;
+end
+fprintf('done \n')
+
+
+
+end
