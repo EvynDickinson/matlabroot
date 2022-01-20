b34f89ab2036dbc5b7bede11563eecbe4dbc0fec
EvynDickinson <evyndickinson@gmail.com>
2022-01-20 15:47:30 -0500

    quad 3

--- Functions and Code/pullFoodColor.m
+++ Functions and Code/pullFoodColor.m
@@ -1,44 +1,65 @@
 
 
-function kolor = pullFoodColor(foodString)
-% kolor = pullFoodColor(foodString)
+function [kolor,num] = pullFoodColor(foodString)
+% [kolor,num] = pullFoodColor(foodString)
+% kolor : [R G B]
+% num : 1 - plant | 2 - yeast | 3 - empty
+% 
 
 switch foodString
-    case 'Yeast'
+    case {'Yeast', 'yeast'}
         colorname = 'gold';
-    case 'Plant'
+        num = 2;
+    case {'Plant', 'plant'}
         colorname = 'green';
-    case 'Empty'
+        num = 1;
+    case {'Empty', 'empty'}
         colorname = 'grey';
+        num = 3;
     % SPECIFIC VARIETIES OF PLANT FOOD:
     case 'Plant_827'
         colorname = 'palegreen';
+        num = 1;
     case {'Plant_91', 'Plant9_1', 'Plant9_1C'}
         colorname = 'Darkgreen';
+        num = 1;
     case 'Plant9_20_A'
         colorname = 'Chartreuse';
+        num = 1;
     case {'Plant9_20_B'}
         colorname = 'teal';
+        num = 1;
     case 'Plant9_20_C'
         colorname = 'cyan';
+        num = 1;
     case 'Plant_11_4_21'
         colorname = 'green';
+        num = 1;
     % SPECIFIC VARIETIES OF YEAST FOOD:
     case 'Yeast_11_4_21'
         colorname = 'gold';
+        num = 2;
     case {'Yeast9_1','Yeast_9_1'}
         colorname = 'sandybrown';
+        num = 2;
     case {'Yeast_8_27','Yeast8_27'}
         colorname = 'sienna';
+        num = 2;
     case 'Yeast_9_20'
         colorname = 'maroon';
+        num = 2;
 end
 
 try kolor = Color(colorname);
 catch 
     if ismember('Yeast', foodString)
         kolor = Color('gold');
+        num = 2;
     elseif ismember('Plant', foodString)
         kolor = Color('green');
+        num = 1;
     end
 end
+
+
+
