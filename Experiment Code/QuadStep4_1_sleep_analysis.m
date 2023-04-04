7d2033684b02ec92563829e4a6504d3b466e31f5
evyndickinson <evyndickinson@gmail.com>
2023-04-04 14:23:11 -0400

    sleep analysis

--- Experiment Code/QuadStep4_1_sleep_analysis.m
+++ Experiment Code/QuadStep4_1_sleep_analysis.m
@@ -8,39 +8,39 @@ nbins = 50;
 
 
 %% visualize grid spacing
-nbins = 50;
-i = 1;
-trial = 1;
-vid = 1;
-frame = 1;
-
-% pull info for the first trial:
-dataDate = data(i).T.Date{trial};
-vid_name = data(i).T.ExperimentID{trial};
-vidDir = [baseFolder dataDate '/' vid_name '_'];
-videoPath = [vidDir num2str(vid) '.avi'];
-movieInfo = VideoReader(videoPath); %read in video
-
-% Set axis limits for the selected arena
-x = data(i).data(trial).data.centre(1);
-y = data(i).data(trial).data.centre(2);
-r = data(i).data(trial).data.r;
-xlimit = [x-(r+50),x+(r+50)];
-ylimit = [y-(r+50),y+50+r];
-
-% Plot image of video frame
-fig = figure; set(fig,'pos',[-1030 279 772 1009],'color','k');
-currentImg = rgb2gray(read(movieInfo,frame));
-imshow(currentImg)
-xlim(xlimit); ylim(ylimit);   
-
-% find the 'auto bin' lines
-xedge = linspace(xlimit(1),xlimit(2),nbins+1);
-yedge = linspace(ylimit(1),ylimit(2),nbins+1);
-
-% Plot the bin outline edges:
-h_line(yedge,'yellow','-',0.25) 
-v_line(xedge,'yellow','-',0.25)
+% nbins = 50;
+% i = 1;
+% trial = 1;
+% vid = 1;
+% frame = 1;
+% 
+% % pull info for the first trial:
+% dataDate = data(i).T.Date{trial};
+% vid_name = data(i).T.ExperimentID{trial};
+% vidDir = [baseFolder dataDate '/' vid_name '_'];
+% videoPath = [vidDir num2str(vid) '.avi'];
+% movieInfo = VideoReader(videoPath); %read in video
+% 
+% % Set axis limits for the selected arena
+% x = data(i).data(trial).data.centre(1);
+% y = data(i).data(trial).data.centre(2);
+% r = data(i).data(trial).data.r;
+% xlimit = [x-(r+50),x+(r+50)];
+% ylimit = [y-(r+50),y+50+r];
+% 
+% % Plot image of video frame
+% fig = figure; set(fig,'pos',[-1030 279 772 1009],'color','k');
+% currentImg = rgb2gray(read(movieInfo,frame));
+% imshow(currentImg)
+% xlim(xlimit); ylim(ylimit);   
+% 
+% % find the 'auto bin' lines
+% xedge = linspace(xlimit(1),xlimit(2),nbins+1);
+% yedge = linspace(ylimit(1),ylimit(2),nbins+1);
+% 
+% % Plot the bin outline edges:
+% h_line(yedge,'yellow','-',0.25) 
+% v_line(xedge,'yellow','-',0.25)
 
 
 
@@ -254,7 +254,7 @@ sSpan = 180; %1 minute smoothing
 
 plotData = struct;
 
-% Cluster the flies on food by temperature?
+% Cluster the sleeping flies by temperature
 for i = 1:num.exp  
     temps = unique(data(i).G(1).TR.temps);
     rateIdx = data(i).G(1).TR.rateIdx;
@@ -301,7 +301,7 @@ disp('All finished')
 r = 5; %rows
 c = 3; %columns
 sb(1).idx = [1,2]; %temp timecourse
-sb(2).idx = [4,5,7,8,10,11,13,14]; %distance from food timecourse %TODO: normalize this to something more intuitive? 
+sb(2).idx = [4,5,7,8,10,11,13,14]; %sleeping flies timecourse
 sb(3).idx = 3:c:r*c; %binned distance alignment
 
 LW = 0.75;
@@ -378,7 +378,7 @@ xlabel('temp (\circC)')
 save_figure(fig,[saveDir expGroup ' sleeping flies summary'],fig_type);
 
 
-% FIGURE: Flies on food by heating / cooling
+% FIGURE: Flies sleeping divided by heating / cooling
 equalLim = true;
 LW = 1.5;
 r = 1;
@@ -450,6 +450,317 @@ end
 
 % save figure
 save_figure(fig,[saveDir 'Flies sleeping during heating and cooling'],fig_type);
+
+
+
+%% FIGURE: distance and sleep over time
+% Set params:
+
+clearvars('-except',initial_vars{:})
+
+LW = 1;
+r = 1; %rows
+c = 2; %columns
+plot_err = true;
+[foreColor,backColor] = formattingColors(blkbgd);
+equalLim = true;
+
+% Organize data
+sSpan = 180; %1 minute smoothing
+plotData = struct;
+
+% Cluster the sleeping flies by temperature?
+for i = 1:num.exp  
+    temps = unique(data(i).G(1).TR.temps);
+    rateIdx = data(i).G(1).TR.rateIdx;
+    tempIdx = data(i).G(1).TR.tempIdx;
+    % find rate index
+    heatRate = find(data(i).G(1).TR.rates>0);
+    coolRate = find(data(i).G(1).TR.rates<0);
+    try 
+        holdRate = find(data(i).G(1).TR.rates==0);
+        ntypes = 3;
+    catch
+        ntypes = 2;
+    end
+    
+    for temp = 1:length(temps)
+        for type = 1:ntypes
+            switch type
+                case 1 %heating
+                    g_name = 'increasing';
+                    idxSex = heatRate;
+                case 2 %cooling
+                    g_name = 'decreasing';
+                    idxSex = coolRate;
+                case 3 %holding
+                    g_name = 'holding';
+                    idxSex = holdRate;
+            end
+            % increasing rates:
+            loc = rateIdx==idxSex & tempIdx==temp; %rate and temp align
+            plotData(i).(g_name)(temp,1) = mean(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan'); %avg 
+            plotData(i).(g_name)(temp,2) = std(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan');%./num.trial(i); %err
+        end
+        % Clustered by temp (regardless of heating/cooling)
+        loc = tempIdx==temp; %temp align only
+        plotData(i).temp_all(temp,1) = mean(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan'); %avg 
+        plotData(i).temp_all(temp,2) = std(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan')./num.trial(i);% %err
+    end
+    plotData(i).temps = temps;
+end
+
+
+% Build figure:
+
+fig = getfig('',true); 
+for i = 1:num.exp
+kolor = grouped(i).color;
+    for type = 1:2 %increasing | decreasing 
+        switch type
+            case 2
+                section_type = 'increasing';
+%                 l_style = '-';
+            case 1
+                section_type = 'decreasing';
+%                 l_style = '--';
+        end
+    
+        % COOLING
+        yyaxis left
+        subplot(r,c,type);   hold on
+        % Sleeping Data
+        x = plotData(i).temps;
+        y = plotData(i).(section_type)(:,1);
+        y_err = plotData(i).(section_type)(:,2);
+        loc = isnan(y)|isnan(y_err);
+        x(loc) = [];
+        y(loc) = [];
+        y_err(loc) = [];
+        if plot_err
+            fill_data = error_fill(x, y, y_err);
+            h = fill(fill_data.X, fill_data.Y, kolor, 'EdgeColor','none','HandleVisibility','off');
+            set(h, 'facealpha', 0.2)
+        end
+        plot(x,y,'color',kolor,'linewidth',LW+1,'linestyle',':','Marker','.','MarkerSize',10)
+        
+       
+        %Distance Data 
+        yyaxis right
+        x = grouped(i).(section_type).temps;
+        y = grouped(i).(section_type).avg;
+        y_err = grouped(i).(section_type).err;
+        loc = isnan(y) | isnan(y_err);% remove nans 
+        y(loc) = []; x(loc) = []; y_err(loc) = [];
+    
+        if plot_err
+            fill_data = error_fill(x, y, y_err);
+            h = fill(fill_data.X, fill_data.Y, kolor, 'EdgeColor','none','HandleVisibility','off');
+            set(h, 'facealpha', 0.2)
+        end
+        plot(x,y,'color',kolor,'linewidth',LW+1,'linestyle','-','Marker','none')
+        set(gca,'ydir','reverse')
+    end
+end
+
+% FORMATING AND LABELS
+formatFig(fig,blkbgd,[r,c]);
+[yleft,yright] = deal([]);
+for type = 1:2
+    subplot(r,c,type); 
+    yyaxis left
+        set(gca,'xcolor',foreColor, 'YColor',foreColor)
+        yleft(:,type) = ylim;
+        ylabel('fraction of sleeping flies')
+    yyaxis right
+        set(gca,'YColor',foreColor)
+        yright(:,type) = ylim;
+        ylabel('food proximity (mm)')
+    xlabel('temp (\circC)')
+end
+
+if equalLim 
+    for type = 1:2
+        subplot(r,c,type); 
+        yyaxis left
+        ylim([min(yleft(1,:)), max(yleft(2,:))])
+            yyaxis right
+            ylim([min(yright(1,:)), max(yright(2,:))])
+    end
+end
+
+% save figure
+save_figure(fig,[saveDir 'sleep and distance during heating and cooling'],fig_type);
+
+
+%% FIGURES: experiment separated overlay of distance to food and sleeping flies
+
+clearvars('-except',initial_vars{:})
+sleepColor = Color('cyan');
+distColor = Color('orange');
+LW = 1;
+r = 1; %rows
+c = 2; %columns
+plot_err = true;
+[foreColor,backColor] = formattingColors(blkbgd);
+equalLim = true;
+
+% Organize data
+sSpan = 180; %1 minute smoothing
+plotData = struct;
+
+% Cluster the sleeping flies by temperature?
+for i = 1:num.exp  
+    temps = unique(data(i).G(1).TR.temps);
+    rateIdx = data(i).G(1).TR.rateIdx;
+    tempIdx = data(i).G(1).TR.tempIdx;
+    % find rate index
+    heatRate = find(data(i).G(1).TR.rates>0);
+    coolRate = find(data(i).G(1).TR.rates<0);
+    try 
+        holdRate = find(data(i).G(1).TR.rates==0);
+        ntypes = 3;
+    catch
+        ntypes = 2;
+    end
+    
+    for temp = 1:length(temps)
+        for type = 1:ntypes
+            switch type
+                case 1 %heating
+                    g_name = 'increasing';
+                    idxSex = heatRate;
+                case 2 %cooling
+                    g_name = 'decreasing';
+                    idxSex = coolRate;
+                case 3 %holding
+                    g_name = 'holding';
+                    idxSex = holdRate;
+            end
+            % increasing rates:
+            loc = rateIdx==idxSex & tempIdx==temp; %rate and temp align
+            plotData(i).(g_name)(temp,1) = mean(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan'); %avg 
+            plotData(i).(g_name)(temp,2) = std(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan');%./num.trial(i); %err
+        end
+        % Clustered by temp (regardless of heating/cooling)
+        loc = tempIdx==temp; %temp align only
+        plotData(i).temp_all(temp,1) = mean(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan'); %avg 
+        plotData(i).temp_all(temp,2) = std(mean(grouped(i).sleep.all_sleep(:,loc),2,'omitnan'),'omitnan')./num.trial(i);% %err
+    end
+    plotData(i).temps = temps;
+end
+
+
+% Build figures:
+for i = 1:num.exp
+    
+    fig = getfig('',true); 
+    % kolor = grouped(i).color;
+        for type = 1:2 %increasing | decreasing 
+            switch type
+                case 2
+                    section_type = 'increasing';
+    %                 l_style = '-';
+                case 1
+                    section_type = 'decreasing';
+    %                 l_style = '--';
+            end
+        
+            % COOLING
+            yyaxis left
+            subplot(r,c,type);   hold on
+            % Sleeping Data
+            x = plotData(i).temps;
+            y = plotData(i).(section_type)(:,1);
+            y_err = plotData(i).(section_type)(:,2);
+            loc = isnan(y)|isnan(y_err);
+            x(loc) = [];
+            y(loc) = [];
+            y_err(loc) = [];
+            if plot_err
+                fill_data = error_fill(x, y, y_err);
+                h = fill(fill_data.X, fill_data.Y, sleepColor, 'EdgeColor','none','HandleVisibility','off');
+                set(h, 'facealpha', 0.2)
+            end
+            plot(x,y,'color',sleepColor,'linewidth',LW+1,'linestyle',':','Marker','.','MarkerSize',10)
+            
+            %Distance Data 
+            yyaxis right
+            x = grouped(i).(section_type).temps;
+            y = grouped(i).(section_type).avg;
+            y_err = grouped(i).(section_type).err;
+            loc = isnan(y) | isnan(y_err);% remove nans 
+            y(loc) = []; x(loc) = []; y_err(loc) = [];
+        
+            if plot_err
+                fill_data = error_fill(x, y, y_err);
+                h = fill(fill_data.X, fill_data.Y, distColor, 'EdgeColor','none','HandleVisibility','off');
+                set(h, 'facealpha', 0.2)
+            end
+            plot(x,y,'color',distColor,'linewidth',LW+1,'linestyle','-','Marker','none')
+            set(gca,'ydir','reverse')
+        end
+    
+        % FORMATING AND LABELS
+        formatFig(fig,blkbgd,[r,c]);
+        [yleft,yright] = deal([]);
+        % decreasing temp formatting
+        subplot(r,c,1); 
+        title('Cooling','Color',foreColor)
+        set(gca,'XDir','reverse'); % flip temp direction to match temporal sequence
+        yyaxis left
+        set(gca,'xcolor',foreColor, 'YColor',sleepColor,'TickDir','out')
+        yleft(:,1) = ylim;
+        ylabel('fraction of flies sleeping')
+        xlabel('temp (\circC)')
+        yyaxis right
+        set(gca,'YColor',backColor)
+        yright(:,1) = ylim;
+        % increasing temp formatting
+        subplot(r,c,2); 
+        title('Warming','Color',foreColor)
+        yyaxis left
+        set(gca,'YColor',backColor,'xcolor',foreColor, 'TickDir','out')
+        yleft(:,2) = ylim;
+        yyaxis right
+        set(gca,'YColor',distColor)
+        ylabel('proximity to food (mm)')
+        xlabel('temp (\circC)')
+        yright(:,2) = ylim;
+
+        if equalLim 
+            for type = 1:2
+                subplot(r,c,type); 
+                yyaxis left
+                ylim([min(yleft(1,:)), max(yleft(2,:))])
+                yyaxis right
+                ylim([min(yright(1,:)), max(yright(2,:))])
+            end
+        end
+        
+    % save figure
+    save_figure(fig,[saveDir expNames{i} ' sleep and distance during heating and cooling'],fig_type);
+
+end
+
+%% 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
