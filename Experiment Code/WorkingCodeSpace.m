b48a40612cfd32dedb22fee01c6b07e35b51a665
EvynDickinson <evyndickinson@gmail.com>
2024-09-11 12:57:37 -0400

    becca time shift code

--- Experiment Code/WorkingCodeSpace.m
+++ Experiment Code/WorkingCodeSpace.m
@@ -13,10 +13,10 @@ imshowpair(Img1, Img2,'montage'); title('Paint weathering')
 save_figure(f, 'E:\My Drive\Jeanne Lab\DATA\Analysis\Paint weathering example', '-png');
 
 %% change genotype
-parameters.ArenaA.genotype = 'UAS-Kir2.1_TM6B_A';
-parameters.ArenaB.genotype = 'UAS-Kir2.1_TM2_E';
-parameters.ArenaC.genotype = 'UAS-Kir2.1_TM6B_A';
-parameters.ArenaD.genotype = 'UAS-Kir2.1_TM2_E';
+parameters.ArenaA.genotype = 'UAS-Kir2.1_TM6B_C';
+parameters.ArenaB.genotype = 'UAS-Kir2.1_TM6B_C';
+parameters.ArenaC.genotype = 'UAS-Kir2.1_TM6B_C';
+parameters.ArenaD.genotype = 'UAS-Kir2.1_TM6B_C';
 
 %% get base folder pathway
 % baseFolder = getCloudPath;
