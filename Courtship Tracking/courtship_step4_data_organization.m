cf6ced021ce0cd9a82d54e6a8fb734a3d6cf811a
evyndickinson <evyndickinson@gmail.com>
2024-11-05 15:17:08 -0500

    courtship data analysis

--- Courtship Tracking/courtship_step4_data_organization.m
+++ Courtship Tracking/courtship_step4_data_organization.m
@@ -1,8 +1,8 @@
+%% 
+% tracks (frame, body points, XY, fly)
+% body points (head, center, abdomen, right wing, left wing)
 
 %% TODOs 
-% load data
-% load the parameter files
-% check the number of tracks
 % check that the same fly is aligned across vidoes
 % add position data to the temperature matrix
 % calculate new parameters from the data
@@ -36,104 +36,125 @@ proofedFlag = [];
 for vid = 1:nvids
     filePath = [baseDir, 'compiled_video_' num2str(vid) '.avi.predictions.slp.h5'];
     proofedFilePath = [baseDir, 'compiled_video_' num2str(vid) '.avi.predictions.analysis.h5'];
-    try data(vid).occupancy_matrix = h5read(filePath,'/track_occupancy');
-        data(vid).tracks = h5read(filePath,'/tracks');
-        data(vid).node_names = h5read(filePath, '/node_names');
-        data(vid).track_names = h5read(filePath, '/track_names');
-    catch data(vid).occupancy_matrix = h5read(proofedFilePath,'/track_occupancy');
+    try data(vid).occupancy_matrix = h5read(proofedFilePath,'/track_occupancy');
         data(vid).tracks = h5read(proofedFilePath,'/tracks');
         data(vid).node_names = h5read(proofedFilePath, '/node_names');
         data(vid).track_names = h5read(proofedFilePath, '/track_names');
         proofedFlag(end+1) = vid;
+    catch data(vid).occupancy_matrix = h5read(filePath,'/track_occupancy');
+        data(vid).tracks = h5read(filePath,'/tracks');
+        data(vid).node_names = h5read(filePath, '/node_names');
+        data(vid).track_names = h5read(filePath, '/track_names');
     end
 
     dataIn = true; %log successful data load
 end
 
 node_names = {'head', 'center', 'abdomen', 'right_wing', 'left_wing'}; % currently assuming these are stable across all videos
-
+disp('data loaded')
 
 %% Post proofing cleaning
 
-i = proofedFlag(1);
-x = squeeze(data(i).tracks(:,2,1,:));
-
-% Quality control 1: screening for locations with more than 2 track points
-nanloc = isnan(x);
-a = sum(nanloc,2)>2;
-if sum(a)>0
-    response = questdlg({[num2str(sum(a)) ' rows with 2+ points found.']; 'Replace with nans?'});
-    if strcmpi(response,'Yes')
-        x(a,:) = nan;
-    else 
-        return
-    end  
-end
-
-% Quality control 2: condense multiple tracks into 2
-% Find where columns 1 and 2 have nans, but row sum = 2
-nanloc = isnan(x);
-sum(nanloc)
-loc = isnan(x(:,1)) & sum(~isnan(x),2)==2;
-if sum(loc)>0
-    response = questdlg([num2str(sum(loc)) ' instances found. What would you like to do?'],...
-        '','Visually approve','Nan replace','Cancel','Visually approve');
-    if strcmpi(response,'Visually approve') % option 1: visually approve
-        %TODO
-        % test coordinates 11111[3]11111
-        % If they don't match up, give option to replace with nans
-    elseif strcmpi(response,'Nan replace') % option 2: replace with nans/skip
-        x(loc,:) = nan;
-    else
-        return % option 3: stop and go to sleap
+for vid = 1:length(proofedFlag)
+    i = proofedFlag(vid);
+    x = squeeze(data(i).tracks(:,2,1,:));
+    y = squeeze(data(i).tracks(:,2,2,:));
+    buffersize = 3;
+    
+    % Quality control 1: screening for locations with more than 2 track points
+    nanloc = isnan(x);
+    a = sum(nanloc,2)>2;
+    if sum(a)>0
+        response = questdlg({[num2str(sum(a)) ' rows with 2+ points found.']; 'Replace with nans?'});
+        if strcmpi(response,'Yes')
+            x(a,:) = nan;
+            y(a,:) = nan;
+            data(i).tracks(a,:,:,:) = nan;
+        else 
+            return
+        end  
     end
-
+    
+    % Quality control 2: condense multiple tracks into 2
+    % Find where columns 1 and 2 have nans, but row sum = 2
+    approval = [];
+    nanloc = isnan(x);
+    
+    for flies = 1:2
+        loc = find(isnan(x(:,flies)) & sum(~isnan(x),2)==2);
+    
+        if sum(loc)>0
+            response = questdlg([num2str(length(loc)) ' instances found. What would you like to do?'],...
+                '','Visually approve','Nan replace','Cancel','Visually approve');
+            if strcmpi(response,'Visually approve') % option 1: visually approve
+                for ii = loc
+                    clist = {'blue','magenta'};
+                    % Get x and y values for both flies 3 frames before and after frame of interest
+                    X1 = squeeze(data(i).tracks(ii-buffersize:ii+buffersize,:,1,1));
+                    Y1 = squeeze(data(i).tracks(ii-buffersize:ii+buffersize,:,2,1));
+                    X2 = squeeze(data(i).tracks(ii-buffersize:ii+buffersize,:,1,2));
+                    Y2 = squeeze(data(i).tracks(ii-buffersize:ii+buffersize,:,2,2));
+                    
+                    % Find track that has x or y value outside tracks 1 or 2
+                    mysterypoint = find(~isnan(x(ii,3:end)))+2;
+                    X3 = squeeze(data(i).tracks(ii,:,1,mysterypoint));
+                    Y3 = squeeze(data(i).tracks(ii,:,2,mysterypoint));
+        
+                    fig = getfig(" ",1);
+                    hold on
+                    for frame = 1:size(X1,1)
+                        plotFlySkeleton(fig, X1(frame,:),Y1(frame,:),Color('dodgerblue'),0); % plot Fly 1 positions before and after frame of interest
+                        plotFlySkeleton(fig, X2(frame,:),Y2(frame,:),Color('pink'),0);  % plot Fly 2 positions before and after frame of interest
+                    end
+                    plotFlySkeleton(fig, X3,Y3,Color(clist{flies}),1); % plot mystery fly 
+                    axis square
+        
+                    % Is it the correct fly identity? If yes, save which location,
+                    % which track it came from, and which track it went to
+                    approval(end+1,:) = [strcmp(questdlg('Correct fly identity?'),'Yes'),ii,mysterypoint,flies];
+                    close(fig);
+                end
+            % elseif strcmpi(response,'Nan replace') % option 2: replace with nans/skip
+            %     x(loc,:) = nan;
+            % else
+                return % option 3: stop and go to sleap
+            end
+        end
+    end
+    
+    for ii = 1:size(approval,1)
+        if approval(ii,1)==1
+            disp([i,approval(ii,2)])
+            % fill fly track with visually identified fly
+            data(i).tracks(approval(ii,2),:,:,approval(ii,4)) = data(i).tracks(approval(ii,2),:,:,approval(ii,3));
+            % replace extra fly track with nans
+            data(i).tracks(approval(ii,2),:,:,approval(ii,3)) = nan;
+        else
+            % replace all fly tracks at that location with nans
+            data(i).tracks(approval(ii,2),:,:,:) = nan;
+        end
+    end
 end
 
 
-%% Quality control
+% Confirm only 2 data tracks for each video
+for vid = 1:nvids
+    occmat = squeeze(~isnan(data(vid).tracks(:,2,1,:)));
+    occsum = sum(occmat,1);
+    overcount = sum(occsum(3:end));
+    if overcount>0
+        response = questdlg([num2str(overcount) ' remaining points outside of primary tracks. Ignore and delete data?']);
+        if ~strcmp('Yes',response)
+            return
+        end
+    end
+    
+    data(vid).occupancy_matrix = occmat(:,1:2);
+end
 
 ntracks = [];
 for vid = 1:parameters.nVids
-    ntracks(vid) = size(data(vid).occupancy_matrix,1);
-end
-
-
-%% 
-% tracks (frame, body points, XY, fly)
-% body points (head, center, abdomen, right wing, left wing)
-
-%% Find the x-y coordinates for each fly
-
-% Set up empty tracks for each of the body parts that are being labeled?
-w = 0;
-for vid = 1:nvids
-    if isempty(data(vid).occupancy_matrix)
-        continue
-    end
-    w = max([size(data(vid).occupancy_matrix,1),w]);
-end
-
-% load fly locations into the X & Y matrices
-positions = [];
-for i = 1:nBP
-    [positions.(node_names{i}).X, positions.(node_names{i}).Y]  = deal([]);
-end
-for vid = 1:nvids
-    if isempty(data(vid).occupancy_matrix)
-        continue
-    end
-    % ---- fly tracked locations -------
-    for bp = 1:length(data(vid).node_names)
-        raw = squeeze(data(vid).tracks(:,bp,:,:));
-        % x-y coordinates of flies for each frame
-        x_loc = squeeze(raw(:,1,:));
-        y_loc = squeeze(raw(:,2,:));
-        data(vid).(node_names{bp}).x =  x_loc;
-        data(vid).(node_names{bp}).y =  y_loc;
-        positions.(node_names{bp}).X = autoCat(positions.(node_names{bp}).X, x_loc,true);
-        positions.(node_names{bp}).Y = autoCat(positions.(node_names{bp}).Y, y_loc,true);
-    end
+    ntracks(vid) = size(data(vid).occupancy_matrix,2);
 end
 
 
@@ -144,7 +165,7 @@ buff = 4;
 vid_align = false([1,nvids]);
 vid_align(1) = true;
 for vid = 1:nvids-1
-    frame_end = size(data(vid).occupancy_matrix,2);
+    frame_end = size(data(vid).occupancy_matrix,1);
     eROI = frame_end-buff:frame_end;
     sROI = 1:1+buff;
     
@@ -193,8 +214,230 @@ for i = 2:nvids
 end
 disp(state)
 switch_state = ~state;
+parameters.switch_state = switch_state;
+
+% Align fly tracks
+[d1,d2] = deal(struct);
+
+for vid = 1:nvids
+    if switch_state(vid)
+        d1(vid).tracks = data(vid).tracks(:,:,:,2);
+        d2(vid).tracks = data(vid).tracks(:,:,:,1);
+    else
+       d1(vid).tracks = data(vid).tracks(:,:,:,1);
+       d2(vid).tracks = data(vid).tracks(:,:,:,2); 
+    end
+end
+
+[D1.pos,D2.pos] = deal([]);
+
+for vid = 1:nvids
+    D1.pos = [D1.pos; d1(vid).tracks];
+    D2.pos = [D2.pos; d2(vid).tracks];
+end
 
 
+%% Confirming fly identities
+
+% Make male and female matrices
+
+wing = [];
+for sex = 1:2
+    switch sex 
+        case 1
+            x = D1.pos(:,:,1);
+            y = D1.pos(:,:,2);
+        case 2
+             x = D2.pos(:,:,1);
+            y = D2.pos(:,:,2);
+    end
+    for w = 1:2
+        switch w 
+            case 1
+                P1 = [x(:,4),y(:,4)]; % left
+            case 2
+                P1 = [x(:,5),y(:,5)]; % right
+        end
+        
+        P2 = [x(:,2),y(:,2)]; % center
+        P3 = [x(:,3),y(:,3)]; % abdomen
+        
+        % 1: Calculate vectors from P2 to P1 and from P2 to P3
+        v1 = P1 - P2;  % Nx2 matrix, vector from P2 to P1 for each time step
+        v2 = P3 - P2;  % Nx2 matrix, vector from P2 to P3 for each time step
+        
+        % 2: Calculate the dot product of v1 and v2 for each time step
+        dotProduct = v1(:,1) .* v2(:,1) + v1(:,2) .* v2(:,2);
+        
+        % 3: Calculate the magnitudes of v1 and v2 for each time step
+        mag_v1 = sqrt(v1(:,1).^2 + v1(:,2).^2);
+        mag_v2 = sqrt(v2(:,1).^2 + v2(:,2).^2);
+        
+        % 4: Calculate the cosine of the angle for each time step
+        cosTheta = dotProduct ./ (mag_v1 .* mag_v2);
+        
+        % 5: Compute the angle in radians, and then convert to degrees
+        anglesRadians = acos(cosTheta);  % Nx1 vector of angles in radians
+        anglesDegrees = rad2deg(anglesRadians);  % Convert to degrees
+        
+        wing(sex).angle(:,w) = anglesDegrees;
+    end
+end
+
+% compare male and female wing angles
+time = T.time;
+fig = getfig('',true, [1032 300]); 
+hold on
+    plot(time, wing(1).angle(:,1),'color', Color('dodgerblue'),'linewidth', 1) % left wing
+    plot(time, wing(1).angle(:,2),'color', Color('dodgerblue'),'linewidth', 1,'LineStyle','--') % right wing
+    plot(time, wing(2).angle(:,1),'color', Color('deeppink'),'linewidth', 1) % left wing
+    plot(time, wing(2).angle(:,2),'color', Color('deeppink'),'linewidth', 1,'LineStyle','--') % right wing
+xlabel('time (s)')
+ylabel('wing angle (\circ)')
+formatFig(fig, true);
+% save_figure(fig,[baseFolder 'Figures/M and F wing angles'],fig_type);
+
+fig = getfig;
+hold on
+histogram(wing(1).angle(:))
+histogram(wing(2).angle(:),'FaceColor',Color('deeppink'))
+
+% View fly image
+vidname = [baseDir, 'compiled_video_1.avi'];
+vidh = VideoReader(vidname);
+frame = 15;
+
+img = read(vidh,frame);
+
+fig = getfig;
+imshow(img)
+hold on
+plotFlySkeleton(fig, D1.pos(frame,:,1),D1.pos(frame,:,2),Color('dodgerblue'),1); 
+plotFlySkeleton(fig, D2.pos(frame,:,1),D2.pos(frame,:,2),Color('pink'),1); 
+
+%% Save male and female identities + set base parameters
+
+response = questdlg('What is pink?','Fly Sex','Female','Male','Cancel','Female');
+switch response
+    case 'Female'
+        f = D2;
+        f.wing = wing(2);
+        m = D1;
+        m.wing = wing(1);
+        parameters.OGFlyOrder = 'MF';
+    case 'Male'
+        f = D1;
+        f.wing = wing(1);
+        m  = D2;
+        m.wing = wing(2);
+        parameters.OGFlyOrder = 'FM';
+    case 'Cancel'
+        return
+end
+parameters.node_names = node_names;
+
+% --------- write parameters to excel sheet --------- 
+[excelfile, Excel, xlFile] = load_CourtshipExperiments;
+trialID = [dateDir{1},'_',trialDir{1}];
+parameters.trialID = trialID;
+% DATE LOCATION:
+date_loc = (strcmp(excelfile(:,Excel.date),dateDir{1})); % find the rows in the excel sheet that match the current exp date
+% RAMP LOCATION:
+% find the ramp number using the diff between the folder and exp name
+rampnum = trialDir{1}(end);
+try rampnum = str2double(rampnum);
+    expName_short = parameters.expID(1:end-1);
+catch; rampnum = 1;
+    expName_short = parameters.expID;
+end
+ramp_col = excelfile(2:end,Excel.ramp);
+ramp_col = [ramp_col{:}];
+ramploc = [false, (ramp_col==rampnum)];
+% EXP ID LOCATION
+exploc = (strcmp(excelfile(:,Excel.expID),expName_short));
+% find location where these all align: 
+xlRow = find(all([date_loc, exploc,  ramploc'],2));
+
+% write the trial ID into the excel sheet
+writecell({trialID},xlFile,'Sheet','Exp List','Range',[Alphabet(Excel.trialID) num2str(xlRow)]);
+
+% save parameters
+c = [baseFolder,'Trial Data/',trialID,'/'];
+if ~exist(c,'dir')
+    mkdir(c)
+end
+save([c,'basic data.mat'],'T','m','f','parameters')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Find the x-y coordinates for each fly
+
+% Set up empty tracks for each of the body parts that are being labeled?
+w = 0;
+for vid = 1:nvids
+    if isempty(data(vid).occupancy_matrix)
+        continue
+    end
+    w = max([size(data(vid).occupancy_matrix,1),w]);
+end
+
+% load fly locations into the X & Y matrices
+positions = [];
+for i = 1:nBP
+    [positions.(node_names{i}).X, positions.(node_names{i}).Y]  = deal([]);
+end
+for vid = 1:nvids
+    if isempty(data(vid).occupancy_matrix)
+        continue
+    end
+    % ---- fly tracked locations -------
+    for bp = 1:length(data(vid).node_names)
+        raw = squeeze(data(vid).tracks(:,bp,:,:));
+        % x-y coordinates of flies for each frame
+        x_loc = squeeze(raw(:,1,:));
+        y_loc = squeeze(raw(:,2,:));
+        data(vid).(node_names{bp}).x =  x_loc;
+        data(vid).(node_names{bp}).y =  y_loc;
+        positions.(node_names{bp}).X = autoCat(positions.(node_names{bp}).X, x_loc,true);
+        positions.(node_names{bp}).Y = autoCat(positions.(node_names{bp}).Y, y_loc,true);
+    end
+end
 
 
 %% Targeted look at heating and cooling
