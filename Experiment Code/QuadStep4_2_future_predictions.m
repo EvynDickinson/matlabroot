8de8c14b10c65f6602375ea1c99e308a835f77cb
evyndickinson <evyndickinson@gmail.com>
2024-12-06 14:00:58 -0500

    Update QuadStep4_2_future_predictions.m

--- Experiment Code/QuadStep4_2_future_predictions.m
+++ Experiment Code/QuadStep4_2_future_predictions.m
@@ -2,3 +2,7 @@
 %% Future predictions for fly behavior 
 
 
+
+dt = 1; % how long in the past was the temperature predicted from (minutes)
+Ti  =  50; % current time point
+Pt = Ti-dt; % base time for the prediction
