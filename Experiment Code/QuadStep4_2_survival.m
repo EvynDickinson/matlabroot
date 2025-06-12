e89b40fdf467d7ebebc2d6c85066e24ce1544cf3
rebeccaray8 <beccaray333@gmail.com>
2025-06-12 12:20:40 -0400

    survival updates

--- Experiment Code/QuadStep4_2_survival.m
+++ Experiment Code/QuadStep4_2_survival.m
@@ -32,28 +32,49 @@ if isempty(fileIdx)
     return
 end
 
-% Establish date, trial, and base directories
-dateDir = eligible_files(fileIdx(1),1);
-trialDir = eligible_files(fileIdx(1),2); 
-baseDir = [baseFolder, dateDir{:} '\', trialDir{:} '\'];
+% % Establish date, trial, and base directories
+% dateDir = eligible_files(fileIdx(1),1);
+% trialDir = eligible_files(fileIdx(1),2); 
+% baseDir = [baseFolder, dateDir{:} '\', trialDir{:} '\'];
 
+% Pull the protocol name and trials under the selected temp protocol
 tempprotocol = temptype{fileIdx};
 temptrials = strcmp(tempprotocol, excelfile(:,5)); % binary
 
-data = cell2mat([excelfile(temptrials,11:end)]);
-timestmp = cell2mat([excelfile(1,11:end)]); % make dynamic for different lengths of exps
+% Which columns in excel file to plot based on each temp protocol
+switch tempprotocol
+    case 'survival_curve_40C'
+        columns = 11:51;
+    case 'survival_curve_5C'
+        columns = [11:13,15,17,19,23,28,33,38,43,48,53,58];
+end
+
+data = [];
+data.raw = cell2mat([excelfile(temptrials,columns)]);
+data.avg = mean(data.raw,'omitnan');
+timestmp = cell2mat([excelfile(1,columns)]);
 time = timestmp*10;
-avgdeath = mean(data);
+
+
+% Plot number of dead flies overtime
+fig = getfig;
+    plot(time,data.avg,'linewidth',2)
+formatFig(fig, blkbnd);
+    ylim([0 20])
+    xlabel('Time (min)')
+    ylabel('Number of dead flies')
+
+% Plot number of alive flies overtime
+numflies = cell2mat([excelfile(temptrials,10)]);
+aliveflies = numflies - cell2mat([excelfile(temptrials,11:51)]);
+% perc_alive = (aliveflies/numflies)*100;
 
 fig = getfig;
-plot(time,data,'linewidth',2)
-
+    plot(time,aliveflies,'linewidth',2)
 formatFig(fig, blkbnd);
-ylim([0 20])
-xlabel('Time (min)')
-ylabel('Number of dead flies')
-
-
-
+    ylim([0 20])
+    % xlim([0 500])
+    xlabel('Time (min)')
+    ylabel('Number of alive flies')
 
 
