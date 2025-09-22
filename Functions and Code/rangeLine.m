938e98bb05bffbd4081f6e91753bdcf15c967ad5
evyndickinson <evyndickinson@gmail.com>
2025-09-22 12:26:21 -0400

    Update rangeLine.m

--- Functions and Code/rangeLine.m
+++ Functions and Code/rangeLine.m
@@ -1,12 +1,15 @@
 
-function y = rangeLine(fig_handle,offset_percent,subtract)
-% y = rangeLine(fig_handle,offset_percent,subtract)
+function [y, percent_range] = rangeLine(fig_handle,offset_percent,subtract)
+% [y, percent_range] = rangeLine(fig_handle,offset_percent,subtract)
 % get the y value for a point just below the 
 % top of the graph -- used for plotting the
 % range of a stimulus, like the laser onset or odor onset
 % subtract = true (default) if the line wants to 
 % be lower than the current ylims
 % subtract = false if you want to go above current ylimits
+% y =  the new y value for the given offset percentage
+% percent_range = what is the range of the given percent offset (e.g. 5% of
+% the current range)
 %
 % ES Dickinson, University of Washington, 2020
 
@@ -29,5 +32,8 @@ if subtract
 else
     y = yMax+(abs(offset));
 end
+
+percent_range = offset;
+
 
 end
