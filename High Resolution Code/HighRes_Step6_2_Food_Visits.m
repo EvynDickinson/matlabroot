c7b6fbb41c2bb658094cb50084b0eeb0008793f2
EvynDickinson <69465674+EvynDickinson@users.noreply.github.com>
2025-10-15 21:18:20 -0400

    high res updates

--- High Resolution Code/HighRes_Step6_2_Food_Visits.m
+++ High Resolution Code/HighRes_Step6_2_Food_Visits.m
@@ -33,21 +33,38 @@ for trial = 1:num.trials
         FV(trial,sex).ROI = onfoodROI;
         FV(trial,sex).nROI = nOnFood;
         FV(trial,sex).duration = onFoodDuration;
-        
+
+        % WORKING HERE **** subdivide the protocol into the four different types of temp regimes:
+        % temps approaching 25 from each direction **** 
+
+        % subdivide by heating/cooling temperature regime first (then subdivide later)
+        % if start of food visit is during a temp regime it counts 
+        % probably could add a buffer to the turn points later, but this will
+        % suffice for now
+        temp_regimes = {'cooling', 'warming', 'hold'};
+        for tt = 1:3 % cooling, warming, holds
+            type_str = temp_regimes{tt};
+            % find which food visits start in each temp regime
+            locs =  ismember(frame_loc_start, find(data.(type_str)));    
+            % sort into groups for the temp regime types: 
+            FV(trial,sex).(type_str).ROI = onfoodROI(locs,:);
+            FV(trial,sex).(type_str).nROI = size(FV(trial,sex).(type_str).ROI,1);
+            FV(trial,sex).(type_str).duration = onFoodDuration(locs);
+        end
+
+
+
     end
 end
 
-%% TODO: group trends across the flies to see how they trend
+%% TODO: group trends across the flies for the full experiment to see how they trend
 clearvars('-except',initial_var{:})
+foreColor = formattingColors(blkbgd); % get background colors
 
-r = 1;
-c = 2;
 
 % Giant histogram across the two different fly sexes
-fig = getfig('',1); 
-
+fig = getfig('',1); hold on
 % histogram
-subplot(r,c,1); hold on
 for sex = 1:2
     plotData = [];
     for trial = 1:num.trials
@@ -60,10 +77,77 @@ end
 set(gca, 'YScale','log')
 xlabel('food visit duration (s)')
 ylabel('instances (#)')
+formatFig(fig, blkbgd);
 
+save_figure(fig, [figDir 'food visit duration histogram'],fig_type)
+
+% Cumulative visit duration distribution -- find the 95% number and mean
+% for each of the flies in the trial
+% does the 95% number or the mean change across temperature regimes?
 % scatter plot of the 95% cumulative distribution time for food visits --
 % could ask if this changes (and the mean) for each temp regime...
-subplot(r,c,2); hold on
+r = 1;
+c = 2;
+offset = 0.3;
+LW = 1;
+sz = 50;
+fig = getfig('',1);
+
+for sex = 1:2
+    kolor = data.color(sex,:);
+    plotData = [];
+    for trial = 1:num.trials
+        y = FV(trial, sex).duration;
+        [f,x] = ecdf(y);
+        % find closest to 95%
+        all_idx = find(f<=0.95);
+        loc = all_idx(end); % max bin closest but less than 95%
+        visit_dur = x(loc); % duration of visits that encapsulates 95% of visits
+        % find mean visit duration
+        mean_dur = mean(y,'omitnan');
+        plotData = [plotData; visit_dur, mean_dur];
+    end
+    mean_dur = mean(plotData(:,2),'omitnan');
+    mean_CDF = mean(plotData(:,1),'omitnan');
+    x = shuffle_data(linspace(sex-offset+0.1, sex+offset-0.1, size(plotData,1)));
+    
+    % plot 95% cumulative dist. data
+    subplot(r, c, 1)
+    hold on
+    scatter(x, plotData(:,1),sz,kolor,'filled')
+    plot([sex-offset, sex+offset], [mean_CDF, mean_CDF],"Color",kolor, 'linewidth', LW)
+    % plot avg visit duration data
+    subplot(r, c, 2)
+    hold on
+    scatter(x, plotData(:,2),sz,kolor,'filled')
+    plot([sex-offset, sex+offset], [mean_dur, mean_dur],"Color",kolor, 'linewidth', LW)
+end
+% formatting
+formatFig(fig, blkbgd, [r c]);
+subplot(r, c, 1)
+    title('95% CDF','color', foreColor)
+    ylabel('food visit duration (s) capturing 95%')
+    set(gca, 'xcolor', 'none')
+    xlim([0, 2.5])
+subplot(r, c, 2)
+    title('Mean','color', foreColor)
+    ylabel('avg food visit duration (s)')
+    set(gca, 'xcolor', 'none')
+    xlim([0, 2.5])
+
+save_figure(fig, [figDir 'food visit duration and CDF'],fig_type)
+
+% check significance?
+% quick t-test
+
+%% Duration of food visits for each temperature regime...
+clearvars('-except',initial_var{:})
+foreColor = formattingColors(blkbgd); % get background colors
+
+
+% Giant histogram across the two different fly sexes
+fig = getfig('',1); hold on
+% histogram
 for sex = 1:2
     plotData = [];
     for trial = 1:num.trials
@@ -72,16 +156,75 @@ for sex = 1:2
     max(plotData)
     histogram(plotData,'FaceColor',data.color(sex,:),'BinEdges',0:2:120,'FaceAlpha',0.6)
 end
-
+% formatting the figure
+set(gca, 'YScale','log')
+xlabel('food visit duration (s)')
+ylabel('instances (#)')
 formatFig(fig, blkbgd);
 
+save_figure(fig, [figDir 'food visit duration histogram'],fig_type)
 
 % Cumulative visit duration distribution -- find the 95% number and mean
 % for each of the flies in the trial
-
 % does the 95% number or the mean change across temperature regimes?
+% scatter plot of the 95% cumulative distribution time for food visits --
+% could ask if this changes (and the mean) for each temp regime...
+r = 1;
+c = 2;
+offset = 0.3;
+LW = 1;
+sz = 50;
+fig = getfig('',1);
 
-[f,x] = ecdf(plotData);
+for sex = 1:2
+    kolor = data.color(sex,:);
+    plotData = [];
+    for trial = 1:num.trials
+        y = FV(trial, sex).duration;
+        [f,x] = ecdf(y);
+        % find closest to 95%
+        all_idx = find(f<=0.95);
+        loc = all_idx(end); % max bin closest but less than 95%
+        visit_dur = x(loc); % duration of visits that encapsulates 95% of visits
+        % find mean visit duration
+        mean_dur = mean(y,'omitnan');
+        plotData = [plotData; visit_dur, mean_dur];
+    end
+    mean_dur = mean(plotData(:,2),'omitnan');
+    mean_CDF = mean(plotData(:,1),'omitnan');
+    x = shuffle_data(linspace(sex-offset+0.1, sex+offset-0.1, size(plotData,1)));
+    
+    % plot 95% cumulative dist. data
+    subplot(r, c, 1)
+    hold on
+    scatter(x, plotData(:,1),sz,kolor,'filled')
+    plot([sex-offset, sex+offset], [mean_CDF, mean_CDF],"Color",kolor, 'linewidth', LW)
+    % plot avg visit duration data
+    subplot(r, c, 2)
+    hold on
+    scatter(x, plotData(:,2),sz,kolor,'filled')
+    plot([sex-offset, sex+offset], [mean_dur, mean_dur],"Color",kolor, 'linewidth', LW)
+end
+% formatting
+formatFig(fig, blkbgd, [r c]);
+subplot(r, c, 1)
+    title('95% CDF','color', foreColor)
+    ylabel('food visit duration (s) capturing 95%')
+    set(gca, 'xcolor', 'none')
+    xlim([0, 2.5])
+subplot(r, c, 2)
+    title('Mean','color', foreColor)
+    ylabel('avg food visit duration (s)')
+    set(gca, 'xcolor', 'none')
+    xlim([0, 2.5])
+
+save_figure(fig, [figDir 'food visit duration and CDF'],fig_type)
+
+% check significance?
+% quick t-test
+
+
+
 
 
 %% TODO: Plot out the trajectories of the flies from before to after they visit the food
@@ -105,10 +248,10 @@ postD = postD * fps; %
 %% TODO: pull out the trajectory before and after the fly is on food to see
 % what region they came from/go to
 
-%% TODO: compare the behavior between males and females
 
 %% TODO: what is the inter-eating-bout frequency? How does this change or
 % not change across temperature?
+
 
 
 
