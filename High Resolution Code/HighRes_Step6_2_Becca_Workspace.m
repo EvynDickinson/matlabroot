e670824a132885852bb679abffd6f079283b4edf
rebeccaray8 <beccaray333@gmail.com>
2026-01-27 16:20:40 -0500

    Update HighRes_Step6_2_Becca_Workspace.m

--- High Resolution Code/HighRes_Step6_2_Becca_Workspace.m
+++ High Resolution Code/HighRes_Step6_2_Becca_Workspace.m
@@ -1,6 +1,6 @@
 
 
-%% Speed in outer ring
+%% Speed across regions timecourse
 
 % pull out when flies in outer ring = 1
 % pull out speed
@@ -20,8 +20,31 @@ for ii = 1:length(region_name)
     plot(data.time,smooth(m,300,'moving'),"Color",Color(color_list{ii}))
 end
 
-formatFig(fig,blkbgd)
+formatFig(fig,blkbgd);
 legend(region_name)
 ylabel('average speed (mm/s)')
 xlabel('time (min)')
+
+%% Speed across regions within temp regimes scatter plot
+
+% pull out when flies are in each region
+% pull out speed for each temp regime time period
+
+region_name = {'OutterRing','innerFoodQuad','innerEmptyQuad'};
+regime_name = {'WT','WS','CT','CS','SS'};
+color_list = {'MetroPurple','MetroRed','MetroOrange'};
+
+for ii = 1:length(region_name)
+    current_var = data.(region_name{ii});
+    loc = logical(replaceNaN(current_var,0));    
+    speed = data.speed;
+    speed(~loc) = nan;
+    speed_all = [squeeze(speed(:,M,:)),squeeze(speed(:,F,:))];
+    for regime = 1:length(regime_name)
+    current_reg = data.tempbin.(regime_name{regime});
+    loc2 = replaceNaN(current_reg,0);
+    speedR = speed_all;
+    speedR(~loc2) = nan;
+
+end
 
