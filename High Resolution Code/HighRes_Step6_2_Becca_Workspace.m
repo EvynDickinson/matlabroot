c082c02c0a013e7e729c4e257b0171782b2b528e
rebeccaray8 <beccaray333@gmail.com>
2026-02-06 15:10:51 -0500

    Update HighRes_Step6_2_Becca_Workspace.m

--- High Resolution Code/HighRes_Step6_2_Becca_Workspace.m
+++ High Resolution Code/HighRes_Step6_2_Becca_Workspace.m
@@ -282,6 +282,7 @@ end
 save_figure(fig,[figDir 'speed between regions histogram'],fig_type);
 
 %% Speed between regions within threat vs safe periods (not distinguishing warm vs cold)
+
 % pull out when flies are in each region
 % pull out speed for each temp regime time period
 
@@ -374,3 +375,150 @@ end
 
 % Save figure
 save_figure(fig,[figDir 'speed between regions within threat vs safe'],fig_type);
+
+
+%% Select temp tuning curve for each region
+
+clearvars('-except',initial_var{:})
+foreColor = formattingColors(blkbgd); % get background colors
+
+% Select the type of information to plot: 
+[title_str,pName,y_dir,y_lab,nullD,scaler,dType,~,sexSep,ylimits] = ... 
+    PlotParamSelectionHR('Heating and Cooling');
+plot_err = true;
+
+if isempty(title_str)
+    return
+end
+fig_dir = [figDir, ' temp tuning curves/'];
+% set figure folder
+if ~exist(fig_dir, 'dir')
+    mkdir(fig_dir)
+end
+ 
+% ------------- Plotting parameters -------------
+
+% select temp protocol specific plotting features
+autoYLim = false; % Y LIMITS
+if any(isnan(ylimits)) % in case new data is added without specs for axis limits
+    autoYLim = true;
+end
+
+% TODO: update this as new protocols are added to the pipeline
+if strcmp(groupName,'Berlin LTS caviar') % X LIMITS
+    xlimits = [13, 37];
+    autoXLim = false;
+    xPos = [15, 25];  % for the shaded threat region on the plot
+    data_cut_off = [15, 35];
+else
+    autoXLim = true;
+end
+
+region_name = {'OutterRing','innerFoodQuad','innerEmptyQuad'}; % region
+color_list = {'MetroPurple','MetroRed','MetroOrange'}; % color
+nRegions = length(region_name);
+r = 1; % figure rows
+c = 2; % heating and cooling separated plots
+LW = 2; % plotting line width
+FA = 0.35; % SEM shading face alpha level
+
+%% ------------- DATA AND PLOTTING -------------
+fig = getfig('',1,[998 882]); % short and fat: [1230 637]
+hold on
+% pull larger data type group
+for region = 1:nRegions
+    current_var = data.(region_name{region});
+    loc = logical(replaceNaN(current_var,0));    
+    speed = data.(pName);
+    % Replace speed values when fly is not in the wanted region with nans
+    speed(~loc) = nan;
+    % Remove M/F fly dimension - all flies and their speeds together
+    yy = [squeeze(speed(:,M,:)),squeeze(speed(:,F,:))];
+    
+    % if sexSep % data separated per fly or per group of flies
+    %     y_all = [squeeze(yy(:,M,:)), squeeze(yy(:,F,:))];
+    % else 
+    %     y_all = yy;
+    % end
+    x  = data.tempbin.temps; % temp bins
+    nTemps = length(x); % number of temperature bins
+    types  = {'cooling', 'warming'};
+    
+    % Extract and Plot data:
+    for ii = 1:2
+        subplot(r,c,ii); hold on
+            Idx = data.tempbin.(types{ii});
+            rawY = nan([nTemps, 2]); % first col = avg, second = sem
+    
+            % extract the tuning information across the temp bins
+            for tt = 1:nTemps 
+                % skip the temp bin if it's not one included in the protocol
+                % (e.g. for cases where the temp slightly overshoots or
+                % undershoots the value and then we get a 'read' for something
+                % like the 14.5 bin when really there are a small number of
+                % data points due to temp overshoot
+                if x(tt)<data_cut_off(1) ||  x(tt)>data_cut_off(2)
+                    continue
+                end
+                % all the cooling data across the flies that fits this temp bin
+                y = yy(Idx(:,tt),:); 
+    
+                % fill the temp bin data into the appropriate structure
+                raw = mean(y,'omitnan').*scaler; % find cooling fly data 
+                rawY(tt,1) = mean(raw,'omitnan');
+                rawY(tt,2) = sem(raw);
+            end
+    
+            % plot the data: 
+             plot_error_fills(plot_err, x, rawY(:,1), rawY(:,2), Color(color_list{region}), fig_type, FA);
+             plot(x,rawY(:,1),'color', Color(color_list{region}), 'LineWidth', LW)
+    end
+end
+         
+% ------------ formatting ------------
+formatFig(fig, blkbgd,[r,c]);
+matchAxis(fig, true);
+for ii = 1:2
+    subplot(r,c,ii) 
+    title(types{ii},'color', foreColor,'FontAngle','italic')
+    xlabel('temperature (\circC)')
+    if ~autoXLim;  xlim(xlimits);  end
+    if ~autoYLim;  ylim(ylimits);  end
+    set(gca, 'ydir', y_dir)
+    % subplot specific adjustments
+    if ii==1 % cooling
+        set(gca, 'XDir','reverse')
+        ylabel(y_lab)
+    end
+    if ii==2 % warming
+        set(gca, 'YColor', 'none')
+    end
+    h_line(nullD, 'gray', '--',1)
+end
+
+% add shaded area for 'threat' temp region
+for tt = 1:2
+    subplot(r, c, tt)
+    ylimits = ylim;
+    pos = [xPos(1,tt), ylimits(1), range(xPos), range(ylimits)]; % [lower-left X, lower-left Y, X-width, Y-height]
+    rectangle('Position', pos, 'FaceColor', foreColor, ... 
+              'FaceAlpha', 0.1, 'EdgeColor', 'none');
+end
+
+% add a time arrow to the temp axes graphs
+arrow_h = 0.05;
+arrow_x = [0.15, 0.18];
+arrow_y = [arrow_h, arrow_h];
+annotation('textarrow', arrow_x, arrow_y, 'String', 'time ','Color',foreColor,'FontSize',12);
+arrow_x = [0.60, 0.63];
+arrow_y = [arrow_h, arrow_h];
+annotation('textarrow', arrow_x, arrow_y, 'String', 'time ','Color',foreColor,'FontSize',12);
+% add dashes between the two temp region halves
+arrow_x = [0.495, 0.55];
+arrow_y = [0.11, 0.11];
+annotation('line', arrow_x, arrow_y,'Color',foreColor,...
+    'linestyle','--','linewidth', 1.8);
+
+
+% Save the Figure
+save_figure(fig, [fig_dir title_str ' tuning curve between regions']);
