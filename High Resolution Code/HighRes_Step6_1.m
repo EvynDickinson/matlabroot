b9795aae867ee6aa1d5523e84ecc0a681c89e255
EvynDickinson <69465674+EvynDickinson@users.noreply.github.com>
2026-03-04 18:10:22 -0500

    HR grouping

--- High Resolution Code/HighRes_Step6_1.m
+++ High Resolution Code/HighRes_Step6_1.m
@@ -12,7 +12,8 @@ baseFolder = [baseDir,'Trial Data/'];
 % Find files that can be run
 [excelfile, Excel, xlFile] = load_HighResExperiments;
 
-if strcmp(questdlg('Load premade data structure?'),'Yes')
+% How do you want to load the data?
+if strcmp(questdlg('Load existing data structure?'),'Yes')
     FileNames = dir([baseDir 'grouped/']);
     FileNames = {FileNames(3:end).name};
     fileIdx = listdlg('ListString', FileNames,'ListSize',[350,450],'promptstring', 'Select data group to load');
@@ -44,8 +45,9 @@ if strcmp(questdlg('Load premade data structure?'),'Yes')
     end
 else % Generate a new data structure from trial-level-data
     % Find trials that are group ready in the excel file: 
-    switch questdlg('Load options from excel?')
-        case 'Yes'
+    switch questdlg('How do you want to load data?', '', ...
+                'manually from excel', 'structure list', 'Cancel','structure list')
+        case 'manually from excel'
                 loc = cellfun(@isnan,excelfile(2:end,Excel.groupready));
                 loc = ~loc;
                 rownums = find(loc)+1; 
@@ -62,22 +64,10 @@ else % Generate a new data structure from trial-level-data
                         selectedFiles{i} = [eligible_files{fileIdx(i),1} '_' eligible_files{fileIdx(i),2}];
                     end
                 end
-                
-                % pull the list of dates and arenas to be loaded
-                % TODO: need some way to pair these by group and ramp 
-                % dateStr = eligible_files(fileIdx,1);
-                % trialStr = eligible_files(fileIdx,2); 
-    
-                % for now, load all the trials as 'independent' trials
-        case 'No'
-              fileList = dir(baseFolder);
-              fileIdx = listdlg('ListString', {fileList(4:end).name},'ListSize',[350,450],'promptstring', 'Select trial to process');
-                if isempty(fileIdx)
-                    disp('No trials selected')
-                    return
-                end
-                fileIdx = fileIdx + 3; % offset to account for the skipped presentation of the '.' files above
-                selectedFiles = {fileList(fileIdx).name};
+        case 'structure list'
+              % load list of grouped experiment folders and select from those
+              % folderOpts = 
+              % WORKING HERE
     
         case 'Cancel'
             return
