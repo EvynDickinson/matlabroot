67d5daec5d8ef1ffb15ec8a7e07d7ec9d3711511
rebeccaray8 <beccaray333@gmail.com>
2026-04-10 14:56:45 -0400

    Update QuadStep1_2.m

--- Experiment Code/QuadStep1_2.m
+++ Experiment Code/QuadStep1_2.m
@@ -656,7 +656,7 @@ switch response
             %     case 'D'
             %         arena = 4;
             % end
-            arena = 1;
+            arena = 3;
             n = 1; % how many miscounted frames to look at
             offset = flyCount(:,arena)-nflies(arena);
             [~,idx] = sort(offset);
